77a53a2cd21ccd1617bf3ff4d7c80a7466116aa1
1308024507pkh <1978942416@qq.com>
2021-07-19 13:25:55 +0800

    更

--- src/main/java/comsoftware/engine/utils/DefaultRecommend.java
+++ src/main/java/comsoftware/engine/utils/DefaultRecommend.java
@@ -30,6 +30,10 @@ public class DefaultRecommend {
                 "https://img1.dongqiudi.com/fastdfs3/M00/B5/9D/ChOxM1xC4DOACOkDAAAkkQuYMgI583.jpg"));
         recommendList.add(new Recommend(1, 50069499, "内马尔",
                 "https://img1.dongqiudi.com/fastdfs3/M00/B6/8A/ChOxM1xC9B6AX1pgAAAvbsXVufA228.jpg"));
+        recommendList.add(new Recommend(1, 50064414, "格列兹曼",
+                "https://img1.dongqiudi.com/fastdfs5/M00/5B/C4/rB8CCl_RhzqAB3U2AAAcDeCa8dU730.jpg"));
+        recommendList.add(new Recommend(1, 50091010, "萨拉赫",
+                "https://img1.dongqiudi.com/fastdfs5/M00/3F/7D/rB8BO19ciRaAHlVVAAAcrz12iiQ413.jpg"));
         return recommendList;
     }
 }
